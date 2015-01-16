86650b6cf137733ae41acf65e91ddba65db7d2b4
rwg <robin@localhost>
2015-01-16 13:39:59 -0800

    add script to install build prereqs

---
+++ prereqs.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libsdl1.2-dev
+sudo apt-get install liblua5.1-dev
+sudo apt-get install luajit-devel
+sudo apt-get install libluajit-5.1-dev
+sudo apt-get install libsdl.\*-dev
+
